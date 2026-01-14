e4a5ac69d00bcacc09bcd5116632cdd69705b215
elnakhlawi <mahmoudelnakhlawy1@gmail.com>
2026-01-14 16:50:15 +0200

    The children prop making a reusable button

--- src/App.jsx
+++ src/App.jsx
@@ -3,38 +3,62 @@ import "./App.css";
 let message = ["Learn React*", "Apply for jobs", "Invest new income"];
 function App() {
   const [step, setStep] = useState(1);
-const [isOpen,setisOpen]=useState(true)
+  const [isOpen, setisOpen] = useState(true);
   function handlePrevious() {
-  if(step >1){
-      setStep((s)=>s - 1);
-  }
+    if (step > 1) {
+      setStep((step) => step - 1);
+    }
   }
 
   function handleNext() {
-  if(step <message.length){
-      setStep((s)=>s+1);
-  }
+    if (step < message.length) {
+      setStep((step) => step + 1);
+    }
   }
   return (
     <>
-    <button className="close-btn" onClick={()=>setisOpen((is)=>!is)}>
-      &times;
-    </button>
-  { isOpen&& 
-(      <div className="steps">
-        <div className="numbers">
-          <div className={`${step >= 1 ? "active" : ""}`}>1</div>
-          <div className={`${step >= 2 ? "active" : ""}`}>2</div>
-          <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+      <button className="close-btn" onClick={() => setisOpen((is) => !is)}>
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+            <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+            <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+          </div>
+          <p>{` Step number ${step}: ${message[step - 1]}`}</p>
+          <div className="btns">
+            <Button onClick={handlePrevious} width="180px">
+              <span>👈</span>
+              <span>Previous</span>
+            </Button>
+            <Button onClick={handleNext} width="130px">
+              <span>
+                Next <span>👉</span>
+              </span>
+            </Button>
+          </div>
         </div>
-        <p>{` Step number ${step}: ${message[step - 1]}`}</p>
-        <div className="btns">
-          <button onClick={handlePrevious}>Previous </button>
-          <button onClick={handleNext}>Next</button>
-        </div>
-      </div>)}
+      )}
     </>
   );
 }
 
+function Button({ onClick, children, width }) {
+  return (
+    <button
+      style={{
+        display: "flex",
+        alignItems: "center",
+        fontSize: "20px",
+        width: width,
+        justifyContent: "center",
+      }}
+      onClick={onClick}
+    >
+      {children}{" "}
+    </button>
+  );
+}
 export default App;
